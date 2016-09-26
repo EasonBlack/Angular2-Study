40dc9c1182a44efa9f42690a25e9b893d0cf26ec
easn xu <eason820720@gmail.com>
2016-09-26 16:48:17 +0800

    t

---
+++ angular-animation/basic/webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'vendor': './vendor.ts',
+        'app': './main.ts'
+    },
+    output: {
+        publicPath: 'http://localhost:8080/',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+    module: {
+        loaders: [{
+            test: /\.ts/, loaders: ['ts-loader'], exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor']
+        }),
+
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
+};
