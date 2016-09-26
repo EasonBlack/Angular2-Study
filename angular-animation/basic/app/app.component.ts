7d772bf8a3c9256e6f884ab383e342903ace462c
easn xu <eason820720@gmail.com>
2016-09-26 17:50:06 +0800

    animate

--- angular-animation/basic/app/app.component.ts
+++ angular-animation/basic/app/app.component.ts
@@ -1,18 +1,43 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-
+import { Component, trigger, state, animate, transition, style } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app/app.component.html',
-    styleUrls: ['./app/app.component.scss']
+    templateUrl: './app.component.html',
+    styles: [
+        `.alert { margin-top: 10px; }`
+    ],
+    animations: [
+        trigger('visibility', [
+            state('shown', style({
+                opacity: 1
+            })),
+            state('hidden', style({
+                opacity: 0
+            })),
+            transition('* => *', animate('1000ms ease-in'))
+        ])
+    ],
+    template: `
+      <div class="container">
+        <h1>Animations</h1>
+        <hr>
+            <button type="button" class="btn btn-primary btn-lg" (click)="toggleVisibility()">
+          {{getLabel()}}
+        </button>
+        <div [@visibility]="visibility" class="alert alert-success">
+          Animate good times! Come on!
+        </div>
+      </div>
+      `
 })
-export class AppComponent  {
-    title = 'app works!';
+export class AppComponent {
+    visibility = 'shown';
 
-    constructor() {
-
+    getLabel() {
+        return this.visibility == 'shown' ? 'Hide' : 'Show';
     }
 
-
-
+    toggleVisibility() {
+        this.visibility = this.visibility == 'shown' ? 'hidden' : 'shown';
+    }
 }
