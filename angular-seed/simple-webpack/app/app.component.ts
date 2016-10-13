03e05d9fd2b22a1835dd1eec80fcff9533adbca1
easonblack <eason820720@163.com>
2016-10-13 17:40:12 +0800

    s

---
+++ angular-seed/simple-webpack/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: `
+    <div>This is My App</div>
+    `
+})
+export class AppComponent {
+    constructor() {
+
+    }
+}
