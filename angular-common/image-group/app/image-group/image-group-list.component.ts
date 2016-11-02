42634ba30084eb0080c4c1b5a697b8ecc2dc78ec
easonblack <eason820720@163.com>
2016-11-02 10:59:39 +0800

    m

--- angular-common/image-group/app/image-group/image-group-list.component.ts
+++ angular-common/image-group/app/image-group/image-group-list.component.ts
@@ -2,38 +2,62 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 @Component({
     selector: 'image-group-list',
     template: `
-    <ul class="image-group__list">
-       <li class="image-group__image_item" *ngFor="let m of image_list" (click)="selectIt(m)">
-            <img src="{{m}}"/>
-       </li>
-       <li class="image-group__button_item">
-            <a class="image-group__button_add">
+    <div class="image-group__list-container">
+       <div class="image-group__more_item image-group__more_left" *ngIf="listOffset < 0" (click)="moveLeft()">
+               <a class="image-group__button_more">
+                  <span class="glyphicon glyphicon-menu-left"></span>
+               </a>
+        </div>
+        <ul class="image-group__list" [ngStyle]="{'left':  listOffset * 100 + 'px'}">
+           <li class="image-group__image_item" *ngFor="let m of image_list" (click)="selectIt(m)">
+                <img src="{{m}}"/>
+           </li>
+        </ul>
+        <div class="image-group__more_item image-group__more_right" *ngIf="(image_list.length + listOffset) >4" (click)="moveRight()">
+               <a class="image-group__button_more">
+                  <span class="glyphicon glyphicon-menu-right"></span>
+               </a>
+        </div>
+        <div class="image-group__button_item" [ngStyle]="{'left': resetLeft()}">
+          <a class="image-group__button_add">
                <span class="glyphicon glyphicon-plus"></span>
-            </a> 
-            <input  accept="image/*" type="file"  (change)="addImage($event)"/>
-      </li>
-    </ul>
+          </a> 
+          <input  accept="image/*" type="file"  (change)="addImage($event)"/>
+        </div>
+    </div>
     `
 })
 export class ImageGroupListComponent {
-    @Input() image_list:any[];
+    @Input() image_list: any[];
     @Output() addImageList = new EventEmitter();
     @Output() selectImage = new EventEmitter();
+    listOffset:any = 0;
     constructor() {
-
     }
 
-    addImage($event:any): void {
+    resetLeft() {
+        if (this.image_list.length > 4) return 400 + 'px';
+        return this.image_list.length * 100 + 'px';
+    }
+
+    addImage($event: any): void {
         console.log($event.target.files[0]);
         var reader = new FileReader();
         reader.readAsDataURL($event.target.files[0]);
         reader.onload = (()=> {
-            //this.image_list.push(reader.result);
             this.addImageList.emit({img: reader.result});
         });
     }
 
-    selectIt(m: any):void {
+    moveLeft() {
+        this.listOffset += 1;
+    }
+
+    moveRight() {
+        this.listOffset += -1;
+    }
+
+    selectIt(m: any): void {
         this.selectImage.emit({img: m});
     }
 }
